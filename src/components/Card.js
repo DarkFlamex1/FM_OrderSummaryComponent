74adf2276e7c0c8b47cce9285da2ea352097f556
Vikram Peddinti <vikram.peddinti@gmail.com>
2021-10-17 21:17:27 -0700

    Added basic component structure + tailwind

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Card extends React.Component {
+    render() {
+      return <h1>Hello, This is the Card Component</h1>;
+    }
+  }
+
+export default Card;
